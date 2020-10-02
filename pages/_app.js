97de5ab6fe089a6c9658264af7b4edaa68e0249b
Jonathan Guadamuz <guadamuzjj@gmail.com>
2020-10-02 13:58:45 -0600

    Adding tailwindcss dependency

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/global.css'
+import '../styles/index.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
